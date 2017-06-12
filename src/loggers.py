78750fc0613f6f75a6c96b649c4441dc465e4d82
Jetic Gu <jetic@me.com>
2017-06-12 11:30:40 -0700

    Added model to loggers

--- src/loggers.py
+++ src/loggers.py
@@ -2,6 +2,42 @@ import logging
 
 
 def init_logger(logFile='aligner.log'):
+    # Model
+    logger = logging.getLogger('Model')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(logFile)
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s [%(levelname)s]: %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    # HMM
+    logger = logging.getLogger('HMM')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(logFile)
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s [%(levelname)s]: %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
     # IBM1
     logger = logging.getLogger('IBM1')
     logger.setLevel(logging.DEBUG)
@@ -22,24 +58,6 @@ def init_logger(logFile='aligner.log'):
 
     # MAIN
     logger = logging.getLogger('MAIN')
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(logFile)
-    fh.setLevel(logging.DEBUG)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter(
-        '%(asctime)s %(name)s [%(levelname)s]: %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-
-    # HMM
-    logger = logging.getLogger('HMM')
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
     fh = logging.FileHandler(logFile)
