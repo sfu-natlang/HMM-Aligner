d86c530c8a41b03b57fe8dca7ec555d45db44d23
Jetic Gu <jetic@me.com>
2017-06-22 15:58:26 -0700

    So, 0.3a for config files.

--- src/aligner.py
+++ src/aligner.py
@@ -1,66 +1,145 @@
 #!/usr/bin/env python
-import optparse
 import sys
 import os
 import importlib
+import argparse
+import StringIO
+from ConfigParser import SafeConfigParser
 from loggers import logging, init_logger
 from models.modelChecker import checkAlignmentModel
 from fileIO import loadBitext, loadTritext, exportToFile, loadAlignment
-__version__ = "0.2a"
+__version__ = "0.3a"
 
 
 if __name__ == '__main__':
-    # Parsing the options
-    optparser = optparse.OptionParser()
-    optparser.add_option("-d", "--datadir", dest="datadir",
-                         default="~/Daten/align-data/",
-                         help="data directory (default=~/Daten/align-data/)")
-    optparser.add_option("--train", dest="trainData",
-                         default="train.20k.seg.cln",
-                         help="prefix of data file(default=train.20k.seg.cln)")
-    optparser.add_option("--train-tag", dest="trainTag",
-                         default="train.20k.tags",
-                         help="prefix of tag file(default=train.20k.tags)")
-    optparser.add_option("--test", dest="testData",
-                         default="test.seg.cln",
-                         help="prefix of data file(default=test.seg.cln)")
-    optparser.add_option("--test-tag", dest="testTag",
-                         default="test.tags",
-                         help="prefix of test tag file(default=test.tags)")
-    optparser.add_option("--source", dest="source", default="cn",
-                         help="suffix of source language (default=cn)")
-    optparser.add_option("--target", dest="target", default="en",
-                         help="suffix of target language (default=en)")
-    optparser.add_option("--alignment", dest="alignment",
-                         default="wa",
-                         help="suffix of alignment file(default=wa)")
-    optparser.add_option("-t", "--threshold", dest="threshold", default=0.5,
-                         type="float",
-                         help="threshold for alignment (default=0.5)")
-    optparser.add_option("-n", "--trainSize", dest="trainSize", default=20357,
-                         type="int",
-                         help="Number of sentences to use for training")
-    optparser.add_option("-v", "--testSize", dest="testSize", default=1956,
-                         type="int",
-                         help="Number of sentences to use for testing")
-    optparser.add_option("-i", "--iterations", dest="iter", default=5,
-                         type="int", help="Number of iterations to train")
-    optparser.add_option("-m", "--model", dest="model", default="IBM1Old",
-                         help="model to use, default is IBM1Old")
-    optparser.add_option("-r", "--reference", dest="reference", default="",
-                         help="Location of reference file")
-    optparser.add_option("-o", "--outputToFile", dest="output", default="o.wa",
-                         help="Path to output file")
-    (opts, _) = optparser.parse_args()
+    # Default values:
+    config = {
+        'dataDir': '',
+        'sourceLanguage': '',
+        'targetLanguage': '',
+
+        'trainData': '',
+        'trainDataTag': '',
+        'trainAlignment': '',
+
+        'testData': '',
+        'testDataTag': '',
+        'reference': '',
+
+        'trainSize': 20357,
+        'testSize': 1956,
+        'iterations': 5,
+        'model': "IBM1Old",
+        'output': 'o.wa'
+    }
+
+    configFileDataSection = {
+        'DataDirectory': 'dataDir',
+        'TargetLanguageSuffix': 'targetLanguage',
+        'SourceLanguageSuffix': 'sourceLanguage',
+    }
+
+    configFileTrainSection = {
+        'TextFilePrefix': 'trainData',
+        'TagFilePrefix': 'trainDataTag',
+        'AlignmentFileSuffix': 'trainAlignment'
+    }
+
+    configFileTestSection = {
+        'TextFilePrefix': 'testData',
+        'TagFilePrefix': 'testDataTag',
+        'Reference': 'reference'
+    }
 
     # Initialise logger
     init_logger('aligner.log')
     __logger = logging.getLogger('MAIN')
-    __logger.info("Using model: " + opts.model)
+
+    # Dealing with arguments here
+    if True:  # Adding arguments
+        # Parsing the options
+        ap = argparse.ArgumentParser(
+            description="""SFU HMM Aligner %s""" % __version__)
+        ap.add_argument(
+            "-d", "--datadir", dest="dataDir",
+            help="data directory")
+        ap.add_argument(
+            "--train", dest="trainData",
+            help="prefix of training data file")
+        ap.add_argument(
+            "--test", dest="testData",
+            help="prefix of testing data file")
+        ap.add_argument(
+            "--train-tag", dest="trainDataTag",
+            help="prefix of training tag file")
+        ap.add_argument(
+            "--test-tag", dest="testDataTag",
+            help="prefix of testing tag file")
+        ap.add_argument(
+            "--source", dest="sourceLanguage",
+            help="suffix of source language")
+        ap.add_argument(
+            "--target", dest="targetLanguage",
+            help="suffix of target language")
+        ap.add_argument(
+            "-a", "--alignment", dest="trainAlignment",
+            help="suffix of alignment file")
+        ap.add_argument(
+            "-n", "--trainSize", dest="trainSize", type=int,
+            help="Number of sentences to use for training")
+        ap.add_argument(
+            "-v", "--testSize", dest="testSize", type=int,
+            help="Number of sentences to use for testing")
+        ap.add_argument(
+            "-i", "--iterations", dest="iter", type=int,
+            help="Number of iterations to train")
+        ap.add_argument(
+            "-m", "--model", dest="model",
+            help="model to use, default is IBM1Old")
+        ap.add_argument(
+            "-r", "--reference", dest="reference",
+            help="Location of reference file")
+        ap.add_argument(
+            "-o", "--outputToFile", dest="output",
+            help="Path to output file")
+        ap.add_argument(
+            "-c", "--config", dest="config",
+            help="Path to config file")
+        args = ap.parse_args()
+
+    # Process config file
+    if args.config:
+        # Check local config path
+        if not os.path.isfile(args.config):
+            __logger.error("The config file doesn't exist: %s\n" % args.config)
+            sys.exit(1)
+
+        # Initialise the config parser
+        __logger.info("Reading configurations from file: %s" % (args.config))
+        cp = SafeConfigParser(os.environ)
+        cp.read(args.config)
+
+        # Process the contents of config file
+        for key in configFileDataSection:
+            if cp.get('General', key) != '':
+                config[configFileDataSection[key]] = cp.get('General', key)
+
+        for key in configFileTrainSection:
+            if cp.get('TrainData', key) != '':
+                config[configFileTrainSection[key]] = cp.get('TrainData', key)
+
+        for key in configFileTestSection:
+            if cp.get('TestData', key) != '':
+                config[configFileTestSection[key]] = cp.get('TestData', key)
+
+    # Reset default values to config file
+    ap.set_defaults(**config)
+    args = ap.parse_args()
+    config.update(vars(args))
 
     # Load model
-    __logger.info("Loading model")
-    Model = importlib.import_module("models." + opts.model).AlignmentModel
+    __logger.info("Loading model: " + config['model'])
+    Model = importlib.import_module("models." + config['model']).AlignmentModel
     modelType = checkAlignmentModel(Model)
     if modelType == -1:
         raise TypeError("Invalid Model class")
@@ -68,74 +147,90 @@ if __name__ == '__main__':
 
     aligner = Model()
 
-    if opts.trainData != "":
+    if config['trainData'] != "":
         trainSource = os.path.expanduser(
-            "%s.%s" % (os.path.join(opts.datadir, opts.trainData), opts.source)
+            "%s.%s" % (os.path.join(config['dataDir'], config['trainData']),
+                       config['sourceLanguage'])
         )
         trainTarget = os.path.expanduser(
-            "%s.%s" % (os.path.join(opts.datadir, opts.trainData), opts.target)
+            "%s.%s" % (os.path.join(config['dataDir'], config['trainData']),
+                       config['targetLanguage'])
         )
         if modelType == 1:
             trainBitext = loadBitext(trainSource,
                                      trainTarget,
-                                     opts.trainSize)
-            aligner.train(trainBitext, opts.iter)
+                                     config['trainSize'])
+            aligner.train(trainBitext, config['iterations'])
 
         if modelType == 2:
             trainSourceTag = os.path.expanduser(
-                "%s.%s" % (os.path.join(opts.datadir, opts.trainTag),
-                           opts.source))
+                "%s.%s" % (os.path.join(config['dataDir'],
+                                        config['trainDataTag']
+                                        ), config['sourceLanguage'])
+            )
             trainTargetTag = os.path.expanduser(
-                "%s.%s" % (os.path.join(opts.datadir, opts.trainTag),
-                           opts.target))
+                "%s.%s" % (os.path.join(config['dataDir'],
+                                        config['trainDataTag']
+                                        ), config['targetLanguage'])
+            )
             trainAlignment = os.path.expanduser(
-                "%s.%s" % (os.path.join(opts.datadir, opts.trainData),
-                           opts.alignment))
+                "%s.%s" % (os.path.join(config['dataDir'],
+                                        config['trainData']
+                                        ), config['trainAlignment'])
+            )
             trainFormTritext = loadTritext(trainSource,
                                            trainTarget,
                                            trainAlignment,
-                                           opts.trainSize)
+                                           config['trainSize'])
 
             trainTagTritext = loadTritext(trainSourceTag,
                                           trainTargetTag,
                                           trainAlignment,
-                                          opts.trainSize)
+                                          config['trainSize'])
 
             aligner.train(formTritext=trainFormTritext,
                           tagTritext=trainTagTritext,
-                          iterations=opts.iter)
+                          iterations=config['iterations'])
 
-    if opts.testData != "":
-        testSource = "%s.%s" %\
-            (os.path.join(opts.datadir, opts.testData), opts.source)
-        testTarget = "%s.%s" %\
-            (os.path.join(opts.datadir, opts.testData), opts.target)
+    if config['testData'] != "":
+        testSource = os.path.expanduser(
+            "%s.%s" % (os.path.join(config['dataDir'], config['testData']),
+                       config['sourceLanguage'])
+        )
+        testTarget = os.path.expanduser(
+            "%s.%s" % (os.path.join(config['dataDir'], config['testData']),
+                       config['targetLanguage'])
+        )
 
         if modelType == 1:
-            testBitext = loadBitext(testSource, testTarget, opts.testSize)
+            testBitext = loadBitext(testSource, testTarget, config['testSize'])
             alignResult = aligner.decode(testBitext)
         if modelType == 2:
             testSourceTag = os.path.expanduser(
-                "%s.%s" % (os.path.join(opts.datadir, opts.testTag),
-                           opts.source))
+                "%s.%s" % (os.path.join(config['dataDir'],
+                                        config['testDataTag']
+                                        ), config['sourceLanguage'])
+            )
             testTargetTag = os.path.expanduser(
-                "%s.%s" % (os.path.join(opts.datadir, opts.testTag),
-                           opts.target))
+                "%s.%s" % (os.path.join(config['dataDir'],
+                                        config['testDataTag']
+                                        ), config['targetLanguage'])
+            )
 
             testFormBitext = loadBitext(testSource,
                                         testTarget,
-                                        opts.testSize)
+                                        config['testSize'])
             testTagBitext = loadBitext(testSourceTag,
                                        testTargetTag,
-                                       opts.testSize)
+                                       config['testSize'])
             alignResult = aligner.decode(formBitext=testFormBitext,
                                          tagBitext=testTagBitext)
 
-        if opts.output != "":
-            exportToFile(alignResult, opts.output)
+        if config['output'] != "":
+            exportToFile(alignResult, config['output'])
 
-        if opts.reference != "":
-            reference = loadAlignment(opts.reference)
+        if config['reference'] != "":
+            reference = loadAlignment(config['reference'])
             if aligner.evaluate:
                 if modelType == 1:
                     aligner.evaluate(testBitext, alignResult, reference)
