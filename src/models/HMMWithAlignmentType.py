7f668f1118e4397134b16b7097de758c0da99097
Jetic Gu <jetic@me.com>
2017-07-02 20:46:03 -0700

    HMMWithAlignmentType
    Not sure what I've done, but the score is higher. Wasn't expecting that
    on first try.

--- src/models/HMMWithAlignmentType.py
+++ src/models/HMMWithAlignmentType.py
@@ -19,13 +19,13 @@ from models.IBM1 import AlignmentModel as AlignerIBM1
 from models.modelBase import Task
 from models.HMMBase import AlignmentModelBase as Base
 from evaluators.evaluator import evaluate
-__version__ = "0.4a"
+__version__ = "0.5a"
 
 
 class AlignmentModel(Base):
     def __init__(self):
         self.modelName = "HMMWithAlignmentType"
-        self.version = "0.1b"
+        self.version = "0.2b"
         self.logger = logging.getLogger('HMM')
         self.p0H = 0.3
         self.nullEmissionProb = 0.000005
@@ -58,57 +58,47 @@ class AlignmentModel(Base):
         Base.__init__(self)
         return
 
-    def _beginningOfIteration(self, dataset):
+    def _beginningOfIteration(self, dataset, maxE):
         self.lenDataset = len(dataset)
-        self.c_feh = defaultdict(
-            lambda: [0.0 for h in range(len(self.typeList))])
+        self.gammaEWord = np.zeros(self.t.shape[1])
+        self.gammaBiword = np.zeros(self.t.shape)
+        self.gammaSum_0 = np.zeros(maxE)
+        self.c_feh = np.zeros(self.t.shape + (len(self.typeIndex),))
         return
 
-    def _updateGamma(self, f, e, gamma, alpha, beta, alphaScale):
+    def gamma(self, f, e, alpha, beta, alphaScale, index):
+        fWords = [f[i][index] for i in range(len(f))]
+        eWords = [e[j][index] for j in range(len(e))]
+        gamma = ((alpha * beta).T / alphaScale).T
+        s = self.sProbability(f, e, index)
         for i in range(len(f)):
             for j in range(len(e)):
-                tmpGamma = alpha[i][j] * beta[i][j] / alphaScale[i]
-                gamma[i][j] = tmpGamma
-                c_feh = self.c_feh[(f[i][self.index], e[j][self.index])]
-                for h in range(len(self.typeList)):
-                    c_feh[h] += tmpGamma * self.sProbability(f[i], e[j], h)
+                self.gammaBiword[fWords[i]][eWords[j]] += gamma[i][j]
+                self.gammaEWord[eWords[j]] += gamma[i][j]
+                self.c_feh[fWords[i]][eWords[j]] += gamma[i][j] * s[i][j]
+        self.gammaSum_0[:len(e)] += gamma[0]
+        return gamma
 
-    def _updateEndOfIteration(self, maxE, delta, gammaSum_0, gammaBiword):
+    def _updateEndOfIteration(self, maxE, delta):
+        self.logger.info("End of iteration")
         # Update a
-        for targetLen in self.eLengthSet:
-            a = self.a[targetLen]
-            for prev_j in range(len(a)):
-                for j in range(len(a[prev_j])):
-                    a[prev_j][j] = 0.0
         for Len in self.eLengthSet:
+            deltaSum = np.sum(delta[Len], axis=1) + 1e-37
             for prev_j in range(Len):
-                deltaSum = 0.0
-                for j in range(Len):
-                    deltaSum += delta[Len][prev_j][j]
-                for j in range(Len):
-                    self.a[Len][prev_j][j] = delta[Len][prev_j][j] /\
-                        (deltaSum + 1e-37)
+                self.a[Len][prev_j][:Len] =\
+                    delta[Len][prev_j][:Len] / deltaSum[prev_j]
 
         # Update pi
-        for i in range(maxE):
-            self.pi[i] = gammaSum_0[i] * (1.0 / self.lenDataset)
+        self.pi[:maxE] = self.gammaSum_0[:maxE] / self.lenDataset
 
         # Update t
-        gammaEWord = defaultdict(float)
-        for f, e in gammaBiword:
-            gammaEWord[e] += gammaBiword[(f, e)]
-        self.t.clear()
-        for f, e in gammaBiword:
-            self.t[(f, e)] = gammaBiword[(f, e)] / (gammaEWord[e] + 1e-37)
+        self.t = np.divide(self.gammaBiword, self.gammaEWord)
 
-        s = self.s if self.index == 0 else self.sTag
-        for (f, e) in self.c_feh:
-            c_feh = self.c_feh[(f, e)]
-            sTmp = s[(f, e)]
-            gammaTmp = gammaBiword[(f, e)]
-            for h in range(len(self.typeList)):
-                sTmp[h] = c_feh[h] / gammaTmp
-        self.fe = ()
+        # Update s
+        if self.index == 0:
+            self.s = self.keyDiv(self.c_feh, self.gammaBiword)
+        else:
+            self.sTag = self.keyDiv(self.c_feh, self.gammaBiword)
         return
 
     def endOfBaumWelch(self):
@@ -127,26 +117,31 @@ class AlignmentModel(Base):
                     a[prev_j + targetLen][j] = a[prev_j][j]
         return
 
-    def sProbability(self, f, e, h):
-        fWord, fTag = f
-        eWord, eTag = e
-        if self.fe != (f, e):
-            self.fe, sKey, sTagKey = (f, e), (f[0], e[0]), (f[1], e[1])
-            self.sTmp = self.s[sKey] if sKey in self.s else None
-            self.sTagTmp = self.sTag[sTagKey] if sTagKey in self.sTag else None
-        sTmp = self.sTmp[h] if self.sTmp else 0
-        sTagTmp = self.sTagTmp[h] if self.sTagTmp else 0
-        if self.index == 0:
-            p1 = (1 - self.lambd) * self.typeDist[h] + self.lambd * sTmp
-            p2 = (1 - self.lambd) * self.typeDist[h] + self.lambd * sTagTmp
-            p3 = self.typeDist[h]
-            return self.lambda1 * p1 + self.lambda2 * p2 + self.lambda3 * p3
-        else:
-            return (1 - self.lambd) * self.typeDist[h] + self.lambd * sTagTmp
+    def sProbability(self, f, e, index=0):
+        sTag = np.tile((1 - self.lambd) * self.typeDist, (len(f), len(e), 1))
+
+        for j in range(len(e)):
+            for i in range(len(f)):
+                if f[i][1] < self.sTag.shape[0] and\
+                        e[j][1] < self.sTag.shape[1]:
+                    sTag[i][j] += self.lambd * self.sTag[f[i][1]][e[j][1]]
+        if index == 1:
+            return sTag
+
+        s = np.tile((1 - self.lambd) * self.typeDist, (len(f), len(e), 1))
+        for j in range(len(e)):
+            for i in range(len(f)):
+                if f[i][0] < self.s.shape[0] and e[j][0] < self.s.shape[1]:
+                    s[i][j] += self.lambd * self.s[f[i][0]][e[j][0]]
+
+        return (self.lambda1 * s +
+                self.lambda2 * sTag +
+                np.tile(self.lambda3 * self.typeDist, (len(f), len(e), 1)))
 
     def trainWithIndex(self, dataset, iterations, index):
         self.index = index
         alignerIBM1 = AlignerIBM1()
+        alignerIBM1.sharedLexikon(self)
         alignerIBM1.initialiseBiwordCount(dataset, index)
         alignerIBM1.EM(dataset, iterations, 'IBM1', index)
         self.task.progress("IBM model Trained")
@@ -155,9 +150,9 @@ class AlignmentModel(Base):
         self.logger.info("Initialising HMM")
         self.initialiseBiwordCount(dataset, index)
         if self.index == 1:
-            self.sTag = self.calculateS(dataset, self.fe_count, index)
+            self.sTag = self.calculateS(dataset, index)
         else:
-            self.s = self.calculateS(dataset, self.fe_count, index)
+            self.s = self.calculateS(dataset, index)
         self.t = alignerIBM1.t
         self.logger.info("HMM Initialised, start training")
         self.baumWelch(dataset, iterations=iterations, index=index)
@@ -184,65 +179,45 @@ class AlignmentModel(Base):
         return
 
     def logViterbi(self, f, e):
-        fLen, eLen = len(f), len(e)
         e = deepcopy(e)
+        fLen, eLen = len(f), len(e)
         for i in range(eLen):
             e.append((424242424243, 424242424243))
+        score = np.zeros((fLen, eLen * 2))
+        prev_j = np.zeros((fLen, eLen * 2))
+        s = self.sProbability(f, e)
+        types = np.argmax(s, axis=2)
 
-        score = np.zeros((fLen, eLen * 2, len(self.typeList)))
-        prev_j = np.zeros((fLen, eLen * 2, len(self.typeList)))
-        prev_h = np.zeros((fLen, eLen * 2, len(self.typeList)))
-
-        for j in range(len(e)):
-            tPr = log(self.tProbability(f[0], e[j]))
-            for h in range(len(self.typeList)):
-                score[0][j][h] = log(self.sProbability(f[0], e[j], h)) + tPr
-                if j < len(self.pi) and self.pi[j] != 0:
-                    score[0][j][h] += log(self.pi[j])
-                else:
-                    score[0][j][h] = - sys.maxint - 1
-
-        for i in range(1, fLen):
-            for j in range(len(e)):
-                maxScore = -sys.maxint - 1
-                jPrevBest = -sys.maxint - 1
-                hPrevBest = 0
-                tPr = log(self.tProbability(f[i], e[j]))
-                for jPrev in range(len(e)):
-                    aPrPreLog = self.aProbability(jPrev, j, eLen)
-                    if aPrPreLog == 0:
-                        continue
-                    aPr = log(aPrPreLog)
-                    for h in range(len(self.typeList)):
-                        temp = score[i - 1][jPrev][h] + aPr + tPr
-                        if temp > maxScore:
-                            maxScore = temp
-                            jPrevBest = jPrev
-                            hPrevBest = h
-
-                for h in range(len(self.typeList)):
-                    s = self.sProbability(f[i], e[j], h)
-                    if s != 0:
-                        temp_s = log(s)
-                        score[i][j][h] = maxScore + temp_s
-                        prev_j[i][j][h] = jPrevBest
-                        prev_h[i][j][h] = hPrevBest
+        with np.errstate(invalid='ignore', divide='ignore'):
+            score = np.log(self.tProbability(f, e)) + np.log(np.max(s, axis=2))
+            a = np.log(self.aProbability(eLen))
+        for i in range(fLen):
+            if i == 0:
+                with np.errstate(invalid='ignore', divide='ignore'):
+                    if 2 * eLen <= self.pi.shape[0]:
+                        score[i] += np.log(self.pi[:eLen * 2])
+                    else:
+                        score[i][:self.pi.shape[0]] += np.log(self.pi)
+                        score[i][self.pi.shape[0]:].fill(-sys.maxint)
+            else:
+                tmp = (a.T + score[i - 1]).T
+                bestPrev_j = np.argmax(tmp, axis=0)
+                prev_j[i] = bestPrev_j
+                score[i] += np.max(tmp, axis=0)
 
         maxScore = -sys.maxint - 1
-        best_j = best_h = 0
-        for j in range(len(e)):
-            for h in range(len(self.typeList)):
-                if score[fLen - 1][j][h] > maxScore:
-                    maxScore = score[fLen - 1][j][h]
-                    best_j, best_h = j, h
+        best_j = 0
+        for j in range(eLen * 2):
+            if score[fLen - 1][j] > maxScore:
+                maxScore = score[fLen - 1][j]
+                best_j = j
 
-        trace = [(best_j + 1, best_h), ]
-
-        j, h = best_j, best_h
         i = fLen - 1
+        j = best_j
+        trace = [(j + 1, int(types[i][j]))]
 
         while (i > 0):
-            j, h = int(prev_j[i][j][h]), int(prev_h[i][j][h])
-            trace = [(j + 1, h)] + trace
+            j = int(prev_j[i][j])
             i = i - 1
+            trace = [(j + 1, int(types[i][j]))] + trace
         return trace
